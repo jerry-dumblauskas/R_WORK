b095d7b903afbc816d201ec452fbd5196154e4f9
jerry-dumblauskas <jdumblauskas@gmail.com>
2016-02-29 18:49:31 -0600

    more R

--- art_of_r_programming_script.R
+++ art_of_r_programming_script.R
@@ -1,1 +1,16 @@
+## Variable assignment
 x<-rnorm(10)
+
+## Vectors
+vect<- c(1,2,3,4)
+vect1<-c(5,6)
+xvect<-vect + (vect1*2)
+print(xvect)
+
+## Matrices
+m <- rbind(c(1,4),c(2,2))
+
+## Lists
+Lst <- list(name="Fred", wife="Mary", no.children=3, child.ages=c(4,7,9))
+
+## Functions
