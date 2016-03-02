3a12d90084e10a4f57c745ae62b71fe988fb4238
jerry-dumblauskas <jdumblauskas@gmail.com>
2016-03-01 19:24:36 -0600

    vectors

--- art_of_r_programming_script.R
+++ art_of_r_programming_script.R
@@ -6,11 +6,55 @@ vect<- c(1,2,3,4)
 vect1<-c(5,6)
 xvect<-vect + (vect1*2)
 print(xvect)
-
+print("length of vector using length()")
+print (length(vect))
+print("vectors recycle")
+print(c(1,2,4) + c(6,0,9,20,22))
+  # Vector Indexing
+  # start at 1
+  # vector1[vector2]
+  # negative subscripts mean exclude
+  # : colon gives range of numbers
+  # seq, a general of the : (supports steps)
+  # rep .... rep(x, how many times)
+  # any(vector operation) -- if any are true
+  # all(vector operation) -- if all are true
+  print("functions are vectorized sqrt(1:9) is:")
+  print(sqrt(1:9))
+  print("even + is vectorized '+'(5,4)")
+  print("+"(5,4))
+  # NA is missing, NULL is not exists
+  # NULL is a special R object with no mode.
+  # subset removes NAs
+  print("which returns a positional vector")
+  z <- c(5,2,-3,8)
+  print(which(z*z > 8))
+  # ifelse(b,u,v)
+  # where b is a Boolean vector, and u and v are vectors.
+  # names can be assigned to a vector!  and refed by vec['name']  !!
+  
 ## Matrices
-m <- rbind(c(1,4),c(2,2))
+  print("Matrices!")
+  # A matrix is a vector with two additional attributes: 
+  #the number of rows and the number of columns. Since matrices are vectors, 
+  #they also have modes, such as numeric and character. 
+  #(On the other hand, vectors are not one-column or one-row matrices.)
+  
+  #storage of a matrix is in column-major order
+  m<-matrix(c(1,2,3,4), ncol=2)
+  print (m)
+  
 
 ## Lists
 Lst <- list(name="Fred", wife="Mary", no.children=3, child.ages=c(4,7,9))
 
 ## Functions
+q<-function(x, y)
+{
+  z<-y+x
+}
+
+print(q(44,11))
+## builtin functions
+x<-c(1,2,3,4)
+print (length(x))
